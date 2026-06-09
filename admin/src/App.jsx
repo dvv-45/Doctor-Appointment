815bfe3a93012238565dda116406f4e4c97deefb
dvv-45 <dvishnu8425@gmail.com>
2026-06-09 10:09:47 +0530

    Implement login functionality and add Doctor and Admin dashboard components with routing

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,120 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React from 'react'
+import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import { useContext } from 'react';
+import { Admincontext } from './context/Admincontext';
+import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
+import {Routes,Route} from 'react-router-dom'
+import Dashboard from './pages/Admin/Dashboard';
+import AllAppointments from './pages/Admin/AllAppointments';
+import AddDoctor from './pages/Admin/AddDoctor';
+import Doctorlist from './pages/Admin/Doctorlist';
+import { Doctorcontext } from './context/Doctorcontext';
+import DoctorDashboard from './pages/Doctor/DoctorDashboard';
+import DoctorAppointment from './pages/Doctor/DoctorAppointment';
+import DoctorProfile from './pages/Doctor/DoctorProfile';
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  const {aToken}=useContext(Admincontext)
+  const {dToken}=useContext(Doctorcontext)
 
-  return (
+  return aToken || dToken? (
+    <div className='bg-linear-to-br from-indigo-100 to-purple-300'>
+      <ToastContainer/>
+      <Navbar />
+      <div className='flex items-start'>
+        <Sidebar/>
+        <Routes>
+          {/* admin routes*/ }
+          <Route path='/' element={<></>}/>
+          <Route path='/admin-dashboard' element={<Dashboard/>}   />
+          <Route path='/all-appointments' element={<AllAppointments/>}/>
+           <Route path='/add-doctor' element={<AddDoctor/>}/>
+            <Route path='/doctor-list' element={<Doctorlist/>}/>
+           {/* doctor routes */}
+            <Route path='/doctor-dashboard' element={<DoctorDashboard/>}/>
+             <Route path='/doctor-Appointments' element={<DoctorAppointment/>}/>
+              <Route path='/doctor-profile' element={<DoctorProfile/>}/>
+
+        </Routes>
+      </div>
+    </div>
+  ):(
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+     <Login/>
+      <ToastContainer/>
     </>
   )
 }
