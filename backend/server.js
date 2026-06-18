a5931108d8d08554e64955923ae33ad26f139985
dvv-45 <dvishnu8425@gmail.com>
2026-06-18 13:43:03 +0530

    fix: update CORS configuration to include additional frontend origins

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 
 // Configure CORS to allow requests from both frontend and admin UI
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175','https://doctor-appointment-frontend-2wmr.onrender.com','https://doctor-appointment-admin-2iin.onrender.com'
     ], // <--- ADD 5175 HERE!
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'token', 'dToken','admintoken'],
