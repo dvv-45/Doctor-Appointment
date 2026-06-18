ff072e6b7eb11b490826505e7b159e761a95dc4a
dvv-45 <dvishnu8425@gmail.com>
2026-06-18 09:02:58 +0530

    feat: implement admin, doctor, and user routes with authentication and appointment management functionalities

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import connectcloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminroute.js';
+import doctorRouter from './routes/doctorroute.js';
+import userModel from './models/usermodel.js';
+import userRouter from './routes/userroute.js';
+
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,6 +24,10 @@ app.use(cors({
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'token', 'dToken','admintoken'],
 }));
+
+app.use('/api/admin', adminRouter);
+app.use('/api/doctor', doctorRouter);
+app.use('/api/user', userRouter);
 
 app.get('/', (req, res) => {
     res.send("API working fine ");
